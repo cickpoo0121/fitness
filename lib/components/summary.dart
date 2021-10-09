3624707a9029a6aba342ee49698f8438f771cf7e
Tee <cickpoo0121@gmail.com>
2021-10-09 14:37:28 +0700

    push to git dashboard page

---
+++ lib/components/summary.dart
@@ -0,0 +1,56 @@
+import 'package:fitness_dashboard/constants.dart';
+import 'package:flutter/material.dart';
+
+class Summary extends StatefulWidget {
+  final String titleName;
+  final int value;
+  final String unit;
+  const Summary(
+      {Key? key,
+      required this.titleName,
+      required this.value,
+      required this.unit})
+      : super(key: key);
+
+  @override
+  _SummaryState createState() => _SummaryState();
+}
+
+class _SummaryState extends State<Summary> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          widget.titleName.toUpperCase(),
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: '${widget.value}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: colorText,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: ' ${widget.unit}',
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
